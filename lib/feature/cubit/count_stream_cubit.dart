97067ddf3a6a4cf3e1be39e565c52c59a2fbacb3
KAHFI ZAIN <zkahfi@gmail.com>
2023-12-19 14:18:34 +0700

    Cubit

--- lib/feature/cubit/count_stream_cubit.dart
+++ lib/feature/cubit/count_stream_cubit.dart
@@ -1,37 +1,20 @@
 import 'package:flutter/material.dart';
 
 Widget countStreamCubit(Stream<int> stream) {
-
   print("bulid");
   return StreamBuilder(
       stream: stream,
-      builder: ( context,  snapshot) {
-
-        if (snapshot.hasError) {
-          return Text('error ${snapshot.error}');
-        }
-
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            return const Text(
-              'Not connected to the stream',
-              style: TextStyle(fontSize: 50),
-            );
-          case ConnectionState.waiting:
-            return const Text(
-              'Waiting for data...',
-              style: TextStyle(fontSize: 50),
-            );
-          case ConnectionState.active:
-            return Text(
-              'Data: ${snapshot.data}',
-              style: const TextStyle(fontSize: 50),
-            );
-          case ConnectionState.done:
-            return const Text(
-              'Stream has closed',
-              style: TextStyle(fontSize: 50),
-            );
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text(
+            'Waiting for data...',
+            style: TextStyle(fontSize: 50),
+          );
+        } else {
+          return Text(
+            'Data: ${snapshot.data}',
+            style: const TextStyle(fontSize: 50),
+          );
         }
       });
 }
