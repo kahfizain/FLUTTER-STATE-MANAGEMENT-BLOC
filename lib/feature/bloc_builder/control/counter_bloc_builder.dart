a64ac6c98f1160346bc679138aad76f5b59f5f13
KAHFI ZAIN <zkahfi@gmail.com>
2023-12-19 17:16:14 +0700

    bloc builder

--- lib/feature/bloc_builder/control/counter_bloc_builder.dart
+++ lib/feature/bloc_builder/control/counter_bloc_builder.dart
@@ -3,8 +3,16 @@ import 'package:state_mangement_bloc/feature/bloc_builder/control/control_bloc.d
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 Widget counterBlocBuilder(Stream<int> stream, final ControlBloc controlBloc) {
-  return BlocBuilder(
+  return BlocBuilder<ControlBloc, int>(
       bloc: controlBloc,
+     /* buildWhen: (previous, current){
+        //todo jika current >0 maka tampilkan selain itu tidak tampilkan
+        if(current > 0){
+          return true;
+        }else{
+          return false;
+        }
+      },*/
       builder: (context, state) {
         return Text(
           "$state",
